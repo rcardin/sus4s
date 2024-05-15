89988ac58d27ca684b2021ef9d5345f4aa8997ff
rcardin <riccardo.cardin@gmail.com>
2024-05-15 09:07:51 +0200

    Added remaining ScalaDoc

--- core/src/main/scala/in/rcard/sus4s/sus4s.scala
+++ core/src/main/scala/in/rcard/sus4s/sus4s.scala
@@ -36,28 +36,33 @@ object sus4s {
     *
     * The `structured` function is completely transparent to any exception thrown by the block or by
     * any of the forked tasks.
-   *
-   * <h2>Example</h2>
-   * {{{
-   * val result = structured {
-   *   val job1 = fork {
-   *     Thread.sleep(1000)
-   *     queue.add("job1")
-   *     42
-   *   }
-   *   val job2 = fork {
-   *     Thread.sleep(500)
-   *     queue.add("job2")
-   *     43
-   *   }
-   *   job1.value + job2.value
-   * }
-   * result shouldBe 85
-   * }}}
     *
-    * @param block The block of code to execute in a structured concurrency context
-    * @tparam A The type of the result of the block
-    * @return The result of the block
+    * <h2>Example</h2>
+    * {{{
+    * val result = structured {
+    *   val job1 = fork {
+    *     Thread.sleep(1000)
+    *     queue.add("job1")
+    *     42
+    *   }
+    *   val job2 = fork {
+    *     Thread.sleep(500)
+    *     queue.add("job2")
+    *     43
+    *   }
+    *   job1.value + job2.value
+    * }
+    * result shouldBe 85
+    * }}}
+    *
+    * Structured concurrency is implemented on top of Java Virtual Threads.
+    *
+    * @param block
+    *   The block of code to execute in a structured concurrency context
+    * @tparam A
+    *   The type of the result of the block
+    * @return
+    *   The result of the block
     */
   inline def structured[A](inline block: Suspend ?=> A): A = {
     val _scope = new StructuredTaskScope.ShutdownOnFailure()
@@ -77,6 +82,32 @@ object sus4s {
     }
   }
 
+  /** Forks a new concurrent task executing the given block of code and returning a [[Job]] that
+    * completes with the value of type `A`. The task is executed in a new Virtual Thread using the
+    * given [[Suspend]] context. The job is transparent to any exception thrown by the `block`, which
+    * means it rethrows the exception.
+    *
+    * <h2>Example</h2>
+    * {{{
+    * structured {
+    *   val job1: Job[Int] = fork {
+    *     Thread.sleep(1000)
+    *     queue.add("job1")
+    *     42
+    *   }
+    * }
+    * }}}
+    *
+    * @param block
+    *   The block of code to execute concurrently
+    * @tparam A
+    *   The type of the value returned by the block
+    * @return
+    *   A [[Job]] representing the concurrent task
+    *
+    * @see
+    *   [[structured]]
+    */
   def fork[A](block: Suspend ?=> A): Suspend ?=> Job[A] = {
     val result = new CompletableFuture[A]()
     summon[Suspend].scope.fork(() => {
